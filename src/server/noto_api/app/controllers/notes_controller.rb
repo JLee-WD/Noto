26bde739e15108c2bac6024b53383239a33564ea
justinl <justin.lee26@gmail.com>
2022-01-28 22:23:00 +1100

    Fixed note model and controller, added seeds

--- src/server/noto_api/app/controllers/notes_controller.rb
+++ src/server/noto_api/app/controllers/notes_controller.rb
@@ -1,2 +1,39 @@
 class NotesController < ApplicationController
+  before_action :set_note_by_id, only: [:show, :update, :destroy]
+  before_action :read_notes, only: [:index, :destroy]
+
+  def index
+    render json: @notes
+  end
+  
+  def show
+    render json: @note
+  end
+
+  def create
+    @note = Note.create(title: params[:title],description: params[:description], code: params[:code], public: params[:public])
+    render json: @note
+  end
+
+  def update
+    @note.update(title: params[:title],description: params[:description], code: params[:code], public: params[:public])
+    render json: @note
+  end
+
+  def destroy
+    @note.destroy
+    render json: @notes
+  end
+
+  private
+  
+  def set_note_by_id
+    @note = Note.find(params[:id])
+  end
+  def read_notes
+    @notes = Note.all
+  end
+  def note_params
+    params.require(:note).permit(:title, :description, :code, :public)
+  end
 end
--- src/server/noto_api/db/seeds.rb
+++ src/server/noto_api/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.create(title: "useState()", description: "Use state is used for local state", code: "useState()", public: true)
+Note.create(title: "useEffect()", description: "Use effect is used for side effects", code: "useEffect()", public: true)
+Note.create(title: "useReducer()", description: "Use reducer is used for other functions", code: "useReducer()", public: true)
