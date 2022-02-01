407cb1a0f0e529def804c2f0894260b4094be426
Richard <Ryuukishi@users.noreply.github.com>
2022-02-01 14:47:18 +1100

    Fixed visibility button

--- src/client/noto_client/src/components/VisibilityButton.jsx
+++ src/client/noto_client/src/components/VisibilityButton.jsx
@@ -1,28 +1,22 @@
 import React from "react";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { IconButton, Tooltip } from '@mui/material';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { IconButton, Tooltip } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
-
-const VisibilityButton = (props) => {
-
-  const toggleVisibility = () => {
-    props.setVisible({public: !props.isPublic})
-  }
-
-  return (
-    <>
-    <Tooltip title="Toggle visibility">
-      <IconButton name="public" onClick={toggleVisibility} sx={props.sx}>
-        { props.isPublic ? <VisibilityIcon /> : <VisibilityOffIcon /> }
-      </IconButton>
-    </Tooltip>
-    </>
-  )
-}
+const VisibilityButton = ({ isPublic, toggleVisibility, sx }) => {
+	return (
+		<>
+			<Tooltip title="Toggle visibility">
+				<IconButton name="public" onClick={toggleVisibility} sx={sx}>
+					{isPublic ? <VisibilityIcon /> : <VisibilityOffIcon />}
+				</IconButton>
+			</Tooltip>
+		</>
+	);
+};
 
 export default VisibilityButton;
