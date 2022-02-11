fadb943d30416d268176662d07ac9e0298dc146b
Richard <Ryuukishi@users.noreply.github.com>
2022-02-11 18:28:46 +1100

    Finished writing Rspec tests for the backend

--- src/server/noto_api/spec/api/api_spec.rb
+++ src/server/noto_api/spec/api/api_spec.rb
@@ -1,81 +1,14 @@
 require 'rails_helper'
-require 'faker'
 
 RSpec.describe 'API', type: :request do
 
-    describe 'should fetch notes' do
+    describe 'should not allow fetching notes when user is unauthenticated' do
       subject { get '/notes' }
   
-      it 'returns success' do
+      it 'returns error' do
         subject
         
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(302)
       end
     end
-
-    describe "should fetch all notes", type: :request do
-      before {get '/notes'}
-      it 'returns all notes' do
-          expect(JSON.parse(response.body).size).to eq(Note.count)
-        end
-      it 'returns status code 200' do
-          expect(response).to have_http_status(200)
-        end
-      end
-
-    describe 'should fetch tags', type: :request do
-        subject { get '/tags' }
-    
-        it 'returns success' do
-          subject
-          
-          expect(response).to have_http_status(200)
-        end
-      end
-
-      describe "should fetch all tags", type: :request do
-        before {get '/tags'}
-        it 'returns all tags' do
-            expect(JSON.parse(response.body).size).to eq(Tag.count)
-          end
-        it 'returns status code 200' do
-            expect(response).to have_http_status(200)
-          end
-        end
-
-    # describe "should delete note", type: :request do
-    #     title = Faker::Book.title
-    #     description = Faker::Book.genre
-    #     code = Faker::Lorem.paragraph
-    #     public = true
-    #     note = Note.create(title: title, description: description, code: code, public: public)
-    #     it 'delete returns success' do
-    #       delete '/notes', :note => { :id => note.id } 
-    #       expect(response).to have_http_status(200)
-    #     end
-    #   end
-
-    # describe "#destroy" do
-    #   it "responds to DELETE" do
-    #     title = Faker::Book.title
-    #     description = Faker::Book.genre
-    #     code = Faker::Lorem.paragraph
-    #     public = true
-    #     note = Note.create(title: title, description: description, code: code, public: public)
-    #     delete '/notes', :note => { :id => :note.id }
-    #     expect(response).to have_http_status(200)
-    #   end
-    # end 
-
-    describe "POST note" do
-      subject { post '/notes' }
-      it 'updates note successfully' do
-        title = Faker::Book.title
-        description = Faker::Book.genre
-        code = Faker::Lorem.paragraph
-        public = true
-      post "/notes", :params => { :note => {:title => title, description => description, :code => code, :public => public}}
-      expect(response).to have_http_status(200)
-    end 
-  end 
 end
